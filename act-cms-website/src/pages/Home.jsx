add2906503d7d1fd4154c341ad2a816dad777227
Jessica Nash <janash@vt.edu>
2023-12-16 09:36:42 -0500

    another slight adjustment to phrasing

--- act-cms-website/src/pages/Home.jsx
+++ act-cms-website/src/pages/Home.jsx
@@ -153,7 +153,7 @@ function Home() {
             <List>
               <CustomListItem primary="$3500 stipend each year of the fellowship." />
               <CustomListItem primary="$1500 travel funding each year of the fellowship." />
-              <CustomListItem primary="Mentorship from an ACT-CMS Programming and Computation Mentor and an ACT-CMS curriculum mentor." />
+              <CustomListItem primary="Mentorship from an ACT-CMS Programming and Computation Mentor and an ACT-CMS Curriculum Mentor." />
               <CustomListItem primary="Annual meeting at MolSSI headquarters for training and networking." />
           </List>
           </div>
