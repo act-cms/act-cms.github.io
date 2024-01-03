5d42a59cee5582294dae039e72930698a4591420
Jessica Nash <janash@vt.edu>
2024-01-03 04:37:05 -0500

    update advisory board page to have bios

--- act-cms-website/src/AdvisoryProfiles.jsx
+++ act-cms-website/src/AdvisoryProfiles.jsx
@@ -1,38 +1,76 @@
-import React from 'react';
-import { Grid, Typography, Paper } from '@mui/material';
+import React, { useEffect } from 'react';
+
+import { Grid, Paper } from '@mui/material';
 
 import Headshot from './assets/headshot.svg';
 
+import JayFoley from './assets/advisory_board/foley_jay.jpg';
+import BonnieHall from './assets/advisory_board/hall_bonnie.jpg';
+import GraceStokes from './assets/advisory_board/stokes_grace.jpg';
+
+import ReactMarkdown from 'react-markdown';
+
 const HeadshotComponent = () => {
     return (
-        <img src={Headshot} alt="description" style={{ filter: 'invert(100%)', maxWidth: '120px' }} />
+        <img src={Headshot} alt="description" style={{ filter: 'invert(100%)', maxWidth: '80%' }} />
     )
   }
 
 const profiles = [
-  { name: 'Jay Foley', title: 'University of North Carolina, Charlotte' },
-  { name: 'Bonnie Hall', title: 'Grand View University' },
-  { name: 'Geoff Hutchison', title: 'University of Pittsburgh' },
-  { name: 'Grace Stokes', title: 'Santa Clara University' },
-  { name: 'Katsuyo Thornton', title: 'University of Michigan' },
+  { headshot: JayFoley, markdown_file: '/bios/Jay_Foley.md'},
+  { headshot: BonnieHall, markdown_file: '/bios/Bonnie_Hall.md'},
+  { name: 'Geoff Hutchison', headshot: Headshot, markdown_file: '/bios/Geoff_Hutchison.md' },
+  { headshot: GraceStokes, markdown_file: '/bios/Grace_Stokes.md' },
+  { name: 'Katsuyo Thornton', headshot: Headshot, markdown_file: '/bios/Katsuyo_Thornton.md' },
 ];
 
-const ProfileCard = ({ name, title }) => {
+const ProfileCard = ({ headshot, markdown_file }) => {
+  const [bio, setBio] = React.useState(null);
+
+  useEffect(() => {
+    fetch(markdown_file)
+      .then((response) => response.text())
+      .then((text) => {
+        setBio(text);
+      });
+  }, [markdown_file]);
+
+  const renderHeadshot = () => {
+    if (headshot === Headshot) {
+      return <HeadshotComponent />;
+    } else {
+      return <img src={headshot} alt="Headshot" style={{ maxWidth: '80%', maxHeight: '300px' }} />;
+    }
+  };
+
+
   return (
-    <Paper elevation={3} sx={{ padding: 1, textAlign: 'center' }}>
-        <HeadshotComponent />   
-      <Typography variant="h6">{name}</Typography>
-      <Typography variant="body2">{title}</Typography>
+    <Paper elevation={3} sx={{ padding: 1 }}>
+      <Grid container>
+        <Grid item sm={12} md={3} sx={{ 
+          display: 'flex', 
+          flexDirection: 'column', 
+          justifyContent: 'center', 
+          alignItems: 'center', // Center horizontally for small screens
+          textAlign: 'center' 
+        }}>
+          { renderHeadshot() }
+        </Grid>
+        <Grid item sm={12} md={9} sx={{ textAlign: { sm: 'center', md: 'left' } }}>
+          <ReactMarkdown>{bio}</ReactMarkdown>
+        </Grid>
+      </Grid>
     </Paper>
   );
 };
+
 
 const AdvisoryTeamGrid = () => {
   return (
     <Grid container spacing={4}>
       {profiles.map((profile, index) => (
-        <Grid item xs={12} sm={6} key={index}>
-          <ProfileCard name={profile.name} title={profile.title} />
+        <Grid item xs={12} key={index}>
+          <ProfileCard headshot={profile.headshot} markdown_file={profile.markdown_file} />
         </Grid>
       ))}
     </Grid>
