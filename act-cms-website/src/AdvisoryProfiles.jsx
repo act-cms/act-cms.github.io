dc6cbb71ff028be592bcea65d1e1d645d2ac23e9
Jessica Nash <janash@vt.edu>
2024-01-05 01:51:27 -0500

    move profile card into component

--- act-cms-website/src/AdvisoryProfiles.jsx
+++ act-cms-website/src/AdvisoryProfiles.jsx
@@ -1,6 +1,8 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 
-import { Grid, Paper } from '@mui/material';
+import { Grid } from '@mui/material';
+
+import ProfileCard from './components/ProfileComponets';  
 
 import Headshot from './assets/headshot.svg';
 
@@ -8,14 +10,6 @@ import JayFoley from './assets/advisory_board/foley_jay.jpg';
 import BonnieHall from './assets/advisory_board/hall_bonnie.jpg';
 import GraceStokes from './assets/advisory_board/stokes_grace.jpg';
 import KatsuyoThornton from './assets/advisory_board/thornton_katsuyo.jpg';
-
-import ReactMarkdown from 'react-markdown';
-
-const HeadshotComponent = () => {
-    return (
-        <img src={Headshot} alt="description" style={{ filter: 'invert(100%)', maxWidth: '80%' }} />
-    )
-  }
 
 const profiles = [
   { headshot: JayFoley, markdown_file: '/bios/Jay_Foley.md'},
@@ -25,45 +19,7 @@ const profiles = [
   { headshot: KatsuyoThornton, markdown_file: '/bios/Katsuyo_Thornton.md' },
 ];
 
-const ProfileCard = ({ headshot, markdown_file }) => {
-  const [bio, setBio] = React.useState(null);
 
-  useEffect(() => {
-    fetch(markdown_file)
-      .then((response) => response.text())
-      .then((text) => {
-        setBio(text);
-      });
-  }, [markdown_file]);
-
-  const renderHeadshot = () => {
-    if (headshot === Headshot) {
-      return <HeadshotComponent />;
-    } else {
-      return <img src={headshot} alt="Headshot" style={{ maxWidth: '80%', maxHeight: '300px' }} />;
-    }
-  };
-
-
-  return (
-    <Paper elevation={3} sx={{ padding: 1 }}>
-      <Grid container>
-        <Grid item sm={12} md={3} sx={{ 
-          display: 'flex', 
-          flexDirection: 'column', 
-          justifyContent: 'center', 
-          alignItems: 'center', // Center horizontally for small screens
-          textAlign: 'center' 
-        }}>
-          { renderHeadshot() }
-        </Grid>
-        <Grid item sm={12} md={9} sx={{ textAlign: { sm: 'center', md: 'left' } }}>
-          <ReactMarkdown>{bio}</ReactMarkdown>
-        </Grid>
-      </Grid>
-    </Paper>
-  );
-};
 
 
 const AdvisoryTeamGrid = () => {
---
+++ act-cms-website/src/components/ProfileComponets.jsx
@@ -0,0 +1,56 @@
+import React, { useEffect } from 'react';
+
+import { Grid, Paper } from '@mui/material';
+
+import ReactMarkdown from 'react-markdown';
+
+import Headshot from '../assets/headshot.svg';
+
+
+const HeadshotComponent = () => {
+    return (
+        <img src={Headshot} alt="description" style={{ filter: 'invert(100%)', maxWidth: '80%' }} />
+    )
+  }
+
+const ProfileCard = ({ headshot, markdown_file }) => {
+    const [bio, setBio] = React.useState(null);
+  
+    useEffect(() => {
+      fetch(markdown_file)
+        .then((response) => response.text())
+        .then((text) => {
+          setBio(text);
+        });
+    }, [markdown_file]);
+  
+    const renderHeadshot = () => {
+      if (headshot === Headshot) {
+        return <HeadshotComponent />;
+      } else {
+        return <img src={headshot} alt="Headshot" style={{ maxWidth: '80%', maxHeight: '300px' }} />;
+      }
+    };
+  
+  
+    return (
+      <Paper elevation={3} sx={{ padding: 1 }}>
+        <Grid container>
+          <Grid item sm={12} md={3} sx={{ 
+            display: 'flex', 
+            flexDirection: 'column', 
+            justifyContent: 'center', 
+            alignItems: 'center', // Center horizontally for small screens
+            textAlign: 'center' 
+          }}>
+            { renderHeadshot() }
+          </Grid>
+          <Grid item sm={12} md={9} sx={{ textAlign: { sm: 'center', md: 'left' } }}>
+            <ReactMarkdown>{bio}</ReactMarkdown>
+          </Grid>
+        </Grid>
+      </Paper>
+    );
+  };
+
+  export default ProfileCard;
