c7904d38a2fee9782c228c1f4bde16a567ec76d2
Jessica Nash <janash@vt.edu>
2024-02-23 15:11:40 -0500

    add hutchison bio

--- act-cms-website/src/profiles/AdvisoryProfiles.jsx
+++ act-cms-website/src/profiles/AdvisoryProfiles.jsx
@@ -4,11 +4,12 @@ import JayFoley from '../assets/advisory_board/foley_jay.jpg';
 import BonnieHall from '../assets/advisory_board/hall_bonnie.jpg';
 import GraceStokes from '../assets/advisory_board/stokes_grace.jpg';
 import KatsuyoThornton from '../assets/advisory_board/thornton_katsuyo.jpg';
+import GeoffHutchison from '../assets/advisory_board/hutchison_geoff.jpg';
 
 const profiles = [
   { headshot: JayFoley, markdown_file: '/bios/Jay_Foley.md'},
   { headshot: BonnieHall, markdown_file: '/bios/Bonnie_Hall.md'},
-  { name: 'Geoff Hutchison', headshot: Headshot, markdown_file: '/bios/Geoff_Hutchison.md' },
+  { headshot: GeoffHutchison, markdown_file: '/bios/Geoff_Hutchison.md' },
   { headshot: GraceStokes, markdown_file: '/bios/Grace_Stokes.md' },
   { headshot: KatsuyoThornton, markdown_file: '/bios/Katsuyo_Thornton.md' },
 ];
